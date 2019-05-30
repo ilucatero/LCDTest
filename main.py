df8d94325f17d656cde2d97253d19fdc6496a5bd
ilucatero <wizard_indarkness@hotmail.com>
2019-05-30 12:05:18 +0200

    allow users to select the emty frame character

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from lcd_simulator.lcd_display import LCDDisplay
 def main():
     """ Runs the Lcd Display simulator asking the user the values to display """
     qit = False
+    
+    empty_frame = input("What the character to use as empty frame on display?\nValid options:\n  (space)\n  .\nYour choice:")
+    if not empty_frame in [' ','.']:
+        print("The given value is not valid!\nExiting...")
+        qit = True
+
     while not qit:
         value = input("What number you want to display? [q - to quit] ")
         if value != 'q':
@@ -16,7 +22,7 @@ def main():
             except ValueError:
                 print("That's not an int!")
 
-            dispay = LCDDisplay(empty_space='.')
+            dispay = LCDDisplay(empty_space=empty_frame)
             line = dispay.get_line_to_display(value)
             print(line)
         else:
